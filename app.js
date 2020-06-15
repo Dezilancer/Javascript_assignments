976613822b16005332718eb026fd0adaa892b959
Dezilancer <30356728+Dezilancer@users.noreply.github.com>
2020-06-15 23:59:22 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,404 @@
+﻿// -------- Chapter # 01: ALERTS--------
+
+// Task # 01 --------
+// alert("Hello world !")
+
+
+// Task # 02 --------
+// alert("Error! please enter a valid password")
+
+
+// Task # 03 --------
+// alert("Welcome to JS Land...\nHappy coding!")
+
+
+// Task # 04 --------
+// alert("Welcome to JS Land...")
+// alert("Happy coding!")
+
+
+// Task # 05 --------
+// alert("Hello... I can run JS through my browser's console")
+
+
+// Task # 06 --------
+// Make use of alerts in your new/existing HTML & CSS project.
+
+
+// Task # 07 --------
+// Practice placement of <script></script> element in
+// following sections of your project in exercise 6:
+// a. Head
+// b. Body (before your page’s HTML)
+// c. Body (inside your page’s HTML)
+// d. Body (after your page’s HTML)
+
+// ------------ xxxxxxxxxxxx ------------ xxxxxxxxxxxx ------------
+
+
+
+// -------- Chapter # 02: VARIABLES FOR STRINGS --------
+
+// Task # 01 --------
+// var  username = ""
+
+
+// Task # 02 --------
+// var myName = "Jhone Doe"
+
+
+// Task # 03 --------
+//--a
+// var message;
+//--b
+// message= "Hello World"
+//--c
+// alert(message)
+
+
+// Task # 04 --------
+// var student_name = "Jhone Doe"
+// var age = "15 years old"
+// var course = "Certified Mobile Dovelopment"
+// alert(student_name)
+// alert(age)
+// alert(course)
+
+
+// Task # 05 --------
+// var email;
+// email =  "example@example.com"
+// alert("My email address is " + email)
+
+
+// Task # 06 --------
+// var book;
+// book = "A smarter way to learn JavaScript"
+// alert("I'm trying to learn from the Book " + book)
+
+
+// Task # 07 --------
+// document.write("<h1>Yah! I can wirte HTML content through JavaScript</h1>");
+
+
+// Task # 08 --------
+// var a;
+// a = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬"
+// alert(a)
+
+// ------------ xxxxxxxxxxxx ------------ xxxxxxxxxxxx ------------
+
+
+
+// -------- Chapter # 03: VARIABLES FOR NUMBERS --------
+
+// Task # 01 --------
+// var age;
+// age = 15 
+// alert("I'm " + age + " years old" )
+
+
+
+// Task # 02 --------
+// ------------------
+
+
+// Task # 03 --------
+// var  birthYear;
+// birthYear = 1990
+// document.write( "<h2>My birthday year is " +  birthYear +   "<br> Data type of my declear variable is number </h2>")
+
+
+// Task # 04 --------
+//--a
+// var visitor_name = "John Doe"
+//--b
+// var product_title = "T-shirt"
+//--c
+// var quantity = 5
+// document.write( "<h2>"  + visitor_name + " ordered " + quantity + " " + product_title + "(s) on XYZ Clothing store<h2>")
+
+// ------------ xxxxxxxxxxxx ------------ xxxxxxxxxxxx ------------
+
+
+
+// -------- Chapter # 04: VARIABLE NAMES: LEGAL & ILLEGAL  --------
+
+// Task # 01 --------
+// var firstName = 'John', lastName = "Doe", age = 42;
+
+// Task # 02 --------
+//--5 legal variables
+// $1name,  name1, firstName, _numone, my1stVariable
+
+//--5 illegal variables
+// 1name, break, 123, .variable, true or false
+
+// Task # 03 --------
+//--a
+// document.write("<h1> Rules for naming JS variables <br>  </h1>")
+
+//--b
+// document.write( "<p>variable names can only contains, numbers, $ (dollar sign) and _ (underscore). For Example : $my_1stVariable</p>")
+
+//--c
+// document.write( "<p>Variable names can only contain , numbers, S and _ . For example : Smy_lstVariable Variable must begin with a letter, $ or _ . For example : $name, _name or name</p>")
+
+//--d
+// document.write( "<p> Variable names are case sensitive </p>")
+
+//--e 
+// document.write( "<p> Variable names should not be JS keywords</p>")
+
+// ------------ xxxxxxxxxxxx ------------ xxxxxxxxxxxx ------------
+
+
+
+// -------- Chapter # 05: MATH EXPRESSIONS --------
+
+// Task # 01 --------
+// var a;
+// a = +prompt("Enter first value", "0")
+// b = +prompt("Enter second value", "0")
+// c = a + b
+// document.write("Sum of " + a + " and " + b + " is " + c + "<br>")
+
+// Task # 02 --------
+// d = a - b
+// document.write("Subtraction of " + a + " and " + b + " is " + d + "<br>")
+
+// e = a * b
+// document.write("Multiplication of " + a + " and " + b + " is " + e + "<br>")
+
+// f = a / b
+// document.write("Division of " + a + " and " + b + " is " + f + "<br>")
+
+// g = a % b
+// document.write("Modulus of " + a + " and " + b + " is " + g + "<br>")
+
+
+// Task # 03 --------
+//--a
+// var x;
+
+//--b
+// document.write("Value after variable declaration is " + x + "<br>")
+
+//--c
+// x = 5;
+
+// --d
+// document.write("Initial value: " + x + "<br>")
+
+//--e
+// x = ++x
+
+//--f
+// document.write("Value after increment is: " + x + "<br>")
+
+//--g
+// x = x + 7
+
+//--h
+// document.write("Value after addition is: " + x + "<br>")
+
+//--i
+// x = --x
+
+//--j
+// document.write("Value after decrement is: " + x + "<br>")
+
+//--k
+// x = x % 3
+
+//--l
+// document.write("The remainder is: " + x + "<br>")
+
+
+// Task # 04 --------
+// ticketPrice = 600
+// quantity = 5
+// toalcost = ticketPrice * quantity
+// document.write("Total cost of " + quantity + " ticket(s) to a movie is " + toalcost + " PKR")
+
+
+// Task # 05 --------
+
+//document.write("Table of 4 <br>");
+//for(var i = 1; i <= 10; i++){
+//	document.write("4" + "x" + i + "=" + 4*i + "<br>")}
+
+
+// Task # 06 --------
+
+
+
+// ------------ xxxxxxxxxxxx ------------ xxxxxxxxxxxx ------------
+
+
+
+// -------- Chapter # 06 - 09: MATH EXPRESSIONS --------
+
+// Task # 01 --------
+
+//var a;
+//a = 10
+//document.write("Result: <br> The value of a is:" + a + "<br>.............................................<br><br>")
+
+//a = ++a
+
+//document.write("The value of ++a is:" + a + "<br>")
+//document.write("Now the value of a is:" + a + "<br><br>")
+
+
+//a = a++
+//document.write("The value of a++ is:" + a + "<br>")
+
+//document.write("Now the value of a is:" + a + "<br><br>")
+
+//a = --a
+
+//document.write("The value of --a is:" + a + "<br>")
+//document.write("Now the value of a is:" + a + "<br><br>")
+
+//a = a--
+//document.write("The value of a-- is:" + a + "<br>")
+//document.write("Now the value of a is:" + a + "<br>")
+
+
+// Task # 02 --------
+
+//var a = 2, b = 1;
+//c = --a;
+//document.write("The output of --a is: " + c + "<br>")
+
+//d = --a - --b;
+//document.write("The output of --a - --b is: " + d + "<br>")
+
+//e = --a - --b + ++b;
+//document.write("The output of --a - --b + ++b is: " + e + "<br>")
+
+//varresult = --a - --b + ++b + b--;
+//document.write("The output of --a - --b + ++b + b-- is: " + varresult + "<br> <br>")
+
+//document.write("The result of --a - --b + ++b + b-- is: " + varresult)
+
+
+// Task # 03 --------
+
+//var a = prompt("Enter your name","name")
+//alert("Hello " + a )
+
+
+// Task # 04 --------
+//Not given ----
+
+// Task # 05 --------
+//var a = prompt("Enter your value", 5)
+//var b = prompt("Enter length", 10)
+//document.write("<h2>Table of " + a + "<br></h2>");
+//for(var i = 1; i <= b; i++){
+//	document.write(a + "x" + i + "=" + a*i + "<br>")}
+
+// ------------ xxxxxxxxxxxx ------------ xxxxxxxxxxxx ------------
+
+
+
+// -------- Chapter # 09 - 11: USER INPUT & CONDITIONAL STATEMENT --------
+
+// Task # 01 --------
+//x = prompt("Enter your city name")
+//if (x == "Karachi" || "karachi" || ""KARACHI){
+//	alert("Welcome to city of lights")}
+
+
+//Task # 02 --------
+//x = prompt("Enter your gender")
+//if (x == "male"){
+//	alert("Good Morning Sir.")}
+
+//if (x == "female"){
+//	alert("Good Morning Ma’am.")}
+
+
+//Task # 05 --------
+//var a = 4;
+//if (++a === 5){
+//alert("given condition for variable a is true");}
+
+//var b = 82;
+//if (b++ === 83){
+//alert("given condition for variable b is true");}
+
+//var c = 12;
+//if (c++ === 13){
+//alert("condition 1 is true");}
+//if (c === 13){
+//alert("condition 2 is true");}
+//if (++c < 14){
+//alert("condition 3 is true");}
+//if(c === 14){
+//alert("condition 4 is true");}
+
+//var materialCost = 20000;
+//var laborCost = 2000;
+//var totalCost = materialCost + laborCost;
+//if (totalCost === laborCost + materialCost){
+//alert("The cost equals");}
+
+//if (true){
+//alert("True");}
+//if (false){
+//alert("False");}
+
+
+
+// ------------ xxxxxxxxxxxx ------------ xxxxxxxxxxxx ------------
+
+
+
+// -------- Chapter # 12 - 13: IF…ELSE & ELSE IF STATEMENT, TESTING SET OF CONDITIONS --------
+
+//Task # 01 --------
+
+
+
+
+
+
+
+// ------------ xxxxxxxxxxxx ------------ xxxxxxxxxxxx ------------
+
+
+
+// -------- Chapter # 14 - 16: ARRAYS --------
+
+//Task # 01 --------
+arr = [];
+
+
+
+
+// ------------ xxxxxxxxxxxx ------------ xxxxxxxxxxxx ------------
+
+
+
+// -------- Chapter # 17 - 20: ARRAYS AND LOOP --------
+
+//Task # 04 --------
+//var a = prompt("Enter your value", 0)
+//var b = prompt("Enter length", 0)
+//document.write("<h2>Table of " + a + "<br></h2>");
+//for(var i = 1; i <= b; i++){
+//	document.write(a + "x" + i + "=" + a*i + "<br>")}
+
+
+
+
+
+
+
+
+
+
+
